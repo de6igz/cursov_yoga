8b3e1af38ed15000315727a13b8b38e5c07ccd8e
Шамис Алена <alshamis2004@gmail.com>
2025-06-29 18:02:50 +0300

    header home

---
+++ pages/home.php
@@ -0,0 +1,8 @@
+<?php
+$pageTitle = 'Главная — Yoga Style';
+
+// 1) подключаем шапку
+include __DIR__ . '/templates/header.html';
+
+// 2) вставляем чистый HTML-шаблон
+include __DIR__ . '/templates/main.html';
